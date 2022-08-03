d631e1828793159f645489365af9e5e0d74483a7
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-03 11:57:37 +0100

    created more components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import Header from "./Components/header/Header"
+import Nav from "./Components/nav/Nav";
+import About from "./Components/about/About"
+import Portfolio from "./Components/portfolio/Portfolio";
+import Contact from "./Components/contact/Contact"
+import Experience from "./Components/experience/Experience"
+import Testimonials from "./Components/testimonials/Testimonial"
+import Footer from "./Components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
       
     </div>
   );
