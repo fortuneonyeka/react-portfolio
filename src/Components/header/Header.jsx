92c1bfc2cea964204897c2e2c3ea6a62d42e22f5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-03 14:08:50 +0100

    complete header component

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import CTA from "./CTA"
+import HeaderSocials from "./HeaderSocials"
+import ProfilePic from "../../Assets/WhatsApp Image 2022-01-28 at 10.04.18 PM.jpeg"
 import "./Header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    
+    <header>
+      <div className="container header_container">
+        <h5>Hello I’m</h5>
+        <h1>Ihedoro Fortunatus</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      <CTA/>
+      <HeaderSocials/>
+
+      <div className="me">
+        <img src={ProfilePic} alt="profile-pic" className="profile-pic"/>
+      </div>
+
+      <a href="#contatc" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
