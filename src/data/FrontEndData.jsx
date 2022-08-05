0ced4c93e6f3e4135fff9574cb8c346e26f6227f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-05 16:31:28 +0100

    created experience data

---
+++ src/data/FrontEndData.jsx
@@ -0,0 +1,53 @@
+const Frontend = [
+  {
+    name: "HTML",
+    experience: "experienced"
+  },
+  {
+    name: "Javascript",
+    experience: "experienced"
+  },
+  {
+    name: "React",
+    experience: "experienced"
+  },
+  {
+    name: "Bootstrap",
+    experience: "experienced"
+  },
+  {
+    name: "CSS",
+    experience: "experienced"
+  },
+  {
+    name: "Tailwind",
+    experience: "experienced"
+  },
+  {
+    name: "Typescript",
+    experience: "experienced"
+  },
+  {
+   
+    name: "Storybook",
+    experience: "experienced"
+  },
+  {
+    
+    name: "EEST API",
+    experience: "experienced"
+  },
+  {
+    
+    name: "RAPID API",
+    experience: "experienced"
+  },
+  {
+    
+    name: "Material UI",
+    experience: "experienced"
+  },
+  
+]
+
+export default Frontend
