c8a328be053efe0a3efd79c43496adfe789f1cf5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-05 16:33:42 +0100

    render experiences

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import "./Experience.css"
+import {BsPatchCheckFill} from "react-icons/bs"
+import Frontend from "../../data/FrontEndData"
+import Backend from "../../data/BackEndData"
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>My Tech And Soft Skills</h5>
+      <h2>My Technical Experience</h2>
+
+      <div className="container experience__container">
+        <div className="frontend__experience">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details"> 
+              {Frontend.map((item) => {
+               return(
+                <div>
+                  <BsPatchCheckFill/>
+                <div><h4 key={item.name}>{item.name}</h4></div>
+                <small className="text-light">{item.experience}</small>
+                </div>
+               ) 
+              })}
+              
+              <small>{Frontend.experience}</small>
+            </article>
+          </div>
+        </div>
+        <div className="backend__experience">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+              <article className="experience__details">
+              {Backend.map((item) => {
+               return(
+                  <div>
+                    <BsPatchCheckFill/>
+                    <div><h4 key={item.name}>{item.name}</h4></div>
+                    <small className="text-light">{item.experience}</small>
+                  </div>
+               ) 
+              })}
+              </article>
+          </div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
