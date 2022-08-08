70ae91d34be85091c5892a769cf14e957f9004e9
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-08 10:08:58 +0100

    mapped through project arrays to display porfolio projects

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import "./Portfolio.css"
+import Projects from "../../data/Projects"
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article>
+          {Projects.map((project) => {
+            return (
+              <div>
+             <img src={project.img} alt="Fit App" />
+          <h3>{project.name}</h3>
+          <h3>{project.description}</h3>
+          <a href={project.github}>GitHub</a>
+          <a href={project.live}>Live</a>
+              </div>
+            )
+          })}
+          
+        </article>
+      </div>
+    </section>
   )
 }
 
