5f3a0a59e3f39d21db58f9c7baafa3a58ea15244
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-08 12:21:47 +0100

    refactor portfolio component

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import "./Portfolio.css"
-import Projects from "../../data/Projects"
+import React from "react";
+import "./Portfolio.css";
+import Projects from "../../data/Projects";
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
+   
       <h5>My Recent Projects</h5>
       <h2>Portfolio</h2>
+     
 
-      <div className="container portfolio__container">
-        <article>
-          {Projects.map((project) => {
-            return (
-              <div>
-             <img src={project.img} alt="Fit App" />
-          <h3>{project.name}</h3>
-          <h3>{project.description}</h3>
-          <a href={project.github}>GitHub</a>
-          <a href={project.live}>Live</a>
-              </div>
-            )
-          })}
+      <div  className="container portfolio__container">
+      {Projects.map((project, index) => {
+        return (
           
-        </article>
-      </div>
+            <article className="portfolio__items">
+              <div className="portfolio__item-image">
+                <img src={project.img} alt={project.name} className="project-img"/>
+              </div>
+              <h3>{project.name.toLocaleUpperCase()}</h3>
+              <h5>{project.description}</h5>
+              <a href={project.github} className="btn" target="_blank">
+                GitHub
+              </a>
+              <a
+                href={project.live}
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live
+              </a>
+            </article>
+         
+        );
+      })}
+       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
