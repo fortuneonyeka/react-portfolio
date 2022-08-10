04b0201872e982390d11a9645a74bd2172c019ba
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-10 10:14:52 +0100

    dynamically display contact options

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import "./Contact.css"
+import ContactData from "../../data/ContactData"
+
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          {ContactData.map((contact, index) => {
+            return(
+              <article key={index} className="contact_option">
+            {contact.icon}
+            <h4>{contact.name}</h4>
+            <h5>{contact.address}</h5>
+            <a href={contact.link} target="_blank" rel="noreferrer">Send A Message</a>
+          </article> 
+            )
+          })}
+          
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
