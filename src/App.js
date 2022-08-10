4c406b7dba746558e7802917d8fa181f2d7072cc
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-10 20:49:29 +0100

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import './App.css';
-import Header from "./Components/header/Header"
+import "./App.css";
+import Header from "./Components/header/Header";
 import Nav from "./Components/nav/Nav";
-import About from "./Components/about/About"
+import About from "./Components/about/About";
 import Portfolio from "./Components/portfolio/Portfolio";
-import Contact from "./Components/contact/Contact"
-import Experience from "./Components/experience/Experience"
-import Recomendations from "./Components/recomendations/Recomendations"
+import Contact from "./Components/contact/Contact";
+import Experience from "./Components/experience/Experience";
+import Recomendations from "./Components/recomendations/Recomendations";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Nav/>
-      
-      <About/>
-      <Experience/>
-      <Portfolio/>
-      <Recomendations/>
-      <Contact/>
-     
-      
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Recomendations />
+      <Contact />
     </div>
   );
 }
